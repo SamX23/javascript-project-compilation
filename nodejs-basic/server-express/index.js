4c1f821c91a8e741a1bf1a139771796ccbb01aa2
Sami <skalexsong@gmail.com>
2021-06-14 21:25:49 +0700

    Update : add more example

--- nodejs-basic/server-express/index.js
+++ nodejs-basic/server-express/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+const DATA = {
+  name: "sami",
+  age: 25,
+};
 
 // Endpoint handler
 app.get("/", (req, res) => res.send("Bismillah"));
+
+app.get("/profile", (req, res) => res.json(JSON.stringify(DATA)));
 
 // Turn on server at attached port
 app.listen(PORT, () =>
